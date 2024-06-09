9b0ab7e591fd74fba8d7f132578438b61730293b
Carlos Solorzano <csolorzanoflores@gmail.com>
2024-06-09 15:25:29 -0600

    Initial Commit

---
+++ src/handlers/hello.js
@@ -0,0 +1,10 @@
+exports.handler = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v4.0! Your function executed successfully!'
+    })
+  };
+};
+
+
